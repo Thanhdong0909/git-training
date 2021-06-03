c652c58207fb8f204afa8a03ef620e85d950c8ac
dongpt-2023 <phan.thanh.dong@sun-asterisk.com>
2021-06-03 16:01:25 +0700

    add project covid 19

--- src/App.js
+++ src/App.js
@@ -1,11 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from 'react';
+import moment from 'moment';
+import '@fontsource/roboto';
+
+import CountrySelector from './components/CountrySelector';
+import HighLight from './components/HightLigh';
+import Summary from './components/Summary';
+import {getCountries, getCountryDetail} from './apis/countryAPi';
+import { sortBy } from 'lodash';
+import 'moment/locale/vi';
+import { Container, Typography } from '@material-ui/core';
 
 function App() {
+  const [countries, setCountries] = useState([]);
+  const [countrySelected, setCountrySelected] = useState('');
+  const [reportToday, setReportToday] = useState([]);
+
+  useEffect(() => {
+    getCountries().then((res)=>{
+      const countries = sortBy(res.data, 'Country');
+      setCountries(countries);
+      setCountrySelected('vn');
+    })
+  }, []);
+
+  useEffect(() => {
+    if (countrySelected) {
+      const {Slug} = countries.find((country) => country.ISO2.toLowerCase() === countrySelected)
+      getCountryDetail(Slug).then((res) => {
+        res.data.pop();
+        setReportToday(res.data);
+      })
+    }
+  }, [countrySelected, countries])
+
+  const handleChangeContry = (e) => {
+    setCountrySelected(e.target.value);
+  }
+
   return (
-    <div className="App">
-      <h1>Learing redux - change text</h1>
-    </div>
+    <Container>
+      <Typography variant='h2' component='h2'>
+        Số liệu Covid 19
+      </Typography>
+      <Typography>
+        {moment().format('LLL')}
+      </Typography>
+      <CountrySelector countries={countries} handleOnChange={handleChangeContry} value={countrySelected}/>
+      <HighLight data={reportToday}/>
+      <Summary data={reportToday} countrySelected={countrySelected}/>
+    </Container>
   );
 }
 
---
+++ src/components/charts/HighMaps/index.jsx
@@ -0,0 +1,90 @@
+import React, { useEffect, useRef, useState } from 'react';
+import Highcharts from 'highcharts';
+import HighchartsReact from 'highcharts-react-official';
+import highchartsMap from 'highcharts/modules/map';
+import { cloneDeep } from 'lodash';
+
+// Load Highcharts modules
+highchartsMap(Highcharts);
+
+const initOptions = {
+  chart: {
+    height: '500',
+  },
+  title: {
+    text: null,
+  },
+  mapNavigation: {
+    enabled: true,
+  },
+  colorAxis: {
+    min: 0,
+    stops: [
+      [0.2, '#FFC4AA'],
+      [0.4, '#FF8A66'],
+      [0.6, '#FF392B'],
+      [0.8, '#B71525'],
+      [1, '	#7A0826'],
+    ],
+  },
+  legend: {
+    layout: 'vertical',
+    align: 'right',
+    verticalAlign: 'bottom',
+  },
+  series: [
+    {
+      mapData: {},
+      name: 'Dân số',
+      joinBy: ['hc-key', 'key'],
+    },
+  ],
+};
+
+const HighMaps = ({mapData}) => {
+  const [options, setMapsOption] = useState({});
+  const chartRef = useRef(null);
+  const [configLoaded, setConfigLoaded] = useState(false);
+
+  useEffect(() => {
+    if(mapData && Object.keys(mapData).length) {
+      const fakeData = mapData.features.map((item, index) => ({
+        key: item.properties['hc-key'],
+        value: index,
+      }))
+      setMapsOption({
+        ...initOptions,
+        series: [{
+          ...initOptions.series[0],
+          mapData,
+          data: fakeData,
+        }]
+      });
+      if(!configLoaded) {
+        setConfigLoaded(true);
+      }
+    }
+  },[mapData]);
+
+  useEffect(() => {
+    if(chartRef && chartRef.current) {
+      chartRef.current.chart.series[0].update({
+        mapData,
+      });
+    }
+  },[mapData])
+
+  if (!configLoaded) return null;
+ return (
+  <HighchartsReact
+    highcharts={Highcharts}
+    constructorType='mapChart'
+    options={cloneDeep(options)}
+    ref={chartRef}
+  >
+
+  </HighchartsReact>
+ )
+}
+
+export default React.memo(HighMaps);
